f3373fab9e7f59fe3c708d6232ca1fa54d1e31bf
apanimesh061 <apanimesh061@gmail.com>
2016-03-10 21:57:35 -0500

    changes made

--- src/main/java/TopicApp/EsContext.scala
+++ src/main/java/TopicApp/EsContext.scala
@@ -1,19 +1,23 @@
 package TopicApp
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+
 import org.apache.hadoop.conf.{Configuration => HadoopConfig}
 import org.apache.hadoop.io.{ArrayWritable, MapWritable, NullWritable, Text}
-import org.apache.spark.mllib.linalg.{Vector, Vectors}
-import org.apache.spark.rdd.RDD
+
 import org.elasticsearch.hadoop.mr.EsInputFormat
-import org.json4s.DefaultFormats
+
 import org.json4s.native.Serialization.write
+
+import org.json4s.DefaultFormats
 
 import scala.collection.JavaConversions._
 
 case class EsDocument(id:String,data:Map[String,String])
 
 /**
-  * Created by Animesh Pandey 
+  * Created by Animesh Pandey
   * on 3/10/16.
   */
 class EsContext(sparkConf:HadoopConfig) extends SparkBase {
@@ -29,7 +33,7 @@ class EsContext(sparkConf:HadoopConfig) extends SparkBase {
     )
     val docs = source.map(
       hit => {
-        val doc = Map("ident" -> hit._1.toString) ++ mwToMap(hit._2)
+        val doc = Map("ident" -> hit._1.toString) ++ EsContext.mwToMap(hit._2)
         write(doc)
       }
     )
@@ -44,11 +48,15 @@ class EsContext(sparkConf:HadoopConfig) extends SparkBase {
       classOf[MapWritable]
     )
     source.map(
-      hit => new EsDocument(hit._1.toString, mwToMap(hit._2))
+      hit => new EsDocument(hit._1.toString, EsContext.mwToMap(hit._2))
     )
   }
 
   def shutdown() = sc.stop()
+
+}
+
+object EsContext {
 
   private def mwToMap(mw:MapWritable):Map[String, String] = {
     val m = mw.map(
@@ -73,36 +81,9 @@ class EsContext(sparkConf:HadoopConfig) extends SparkBase {
     m.toMap
   }
 
-  private def mwToMap(mw:MapWritable, fields:Array[String]):Map[String, String] = {
-    val m = mw.map(
-      e => {
-        val k = e._1.toString
-        val v = e._2 match {
-          case _: Text =>
-            e._2.toString
-
-          case writable: ArrayWritable =>
-            val array = writable.get()
-            array.map(
-              item => {
-                if (item.isInstanceOf[NullWritable]) "" else item.asInstanceOf[Text].toString
-              }
-            ).mkString(",")
-
-          case _ => ""
-        }
-        k -> v
-      }
-    )
-    m.toMap
-  }
-
   private def toVector(data:Map[String, String], fields:Array[String]):Vector = {
     val features = data.filter(kv => fields.contains(kv._1)).map(_._2.toDouble)
     Vectors.dense(features.toArray)
   }
 
 }
-
-
-
